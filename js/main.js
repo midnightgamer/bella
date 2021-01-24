2852d6532002201c41ac85a3578d9f673589819a
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2021-01-24 11:31:26 +0530

    section 2 image revel added

--- js/main.js
+++ js/main.js
@@ -34,12 +34,67 @@ function initNavigation() {
         },
         onEnter: ({direction}) => navAnimation(direction),
         onLeaveBack: ({direction}) => navAnimation(direction),
-        markers: true
+        // markers: true
     })
 }
 
 function initHeaderTilt() {
     document.querySelector('header').addEventListener('mousemove', moveImages);
+}
+
+function initHoverRevel() {
+    const sections = gsap.utils.toArray('.rg__column')
+
+
+    sections.forEach(section => {
+
+        //Components that need to be animated
+        section.imageBlock = section.querySelector('.rg__image')
+        section.imageBlockImage = section.querySelector('.rg__image img')
+        section.mask = section.querySelector('.rg__image--mask')
+        section.text = section.querySelector('.rg__text')
+        section.textCopy = section.querySelector('.rg__text--copy')
+        section.textMask = section.querySelector('.rg__text--mask')
+        section.textp = section.querySelector('.rg__text--mask p')
+
+        //    Reset Initial Position
+        gsap.set(section.imageBlock, {yPercent: -101})
+        gsap.set(section.mask, {yPercent: 100})
+
+        gsap.set(section.textMask, {yPercent: -101})
+        gsap.set(section.textp, {yPercent: 100})
+
+        //    add event listener
+        section.addEventListener('mouseenter', createHoverReveal)
+        section.addEventListener('mouseleave', createHoverReveal)
+    })
+}
+
+function createHoverReveal(e) {
+    const {mask, imageBlock, text, textp, textCopy, textMask, imageBlockImage} = e.target;
+    let tl = gsap.timeline({
+        defaults: {
+            duration: 0.7,
+            ease: 'Power4.out'
+        }
+    })
+    if (e.type === 'mouseenter') {
+
+        tl.to([mask, imageBlock, textp, textMask], {yPercent: 0})
+            .from(imageBlockImage, {scale: 1.2, duration: 1.2}, 0)
+            .to(text, {y: () => -textHeight(textCopy) / 2}, 0)
+
+    } else if (e.type === 'mouseleave') {
+
+        tl.to([mask, textp], {yPercent: 100})
+            .to([imageBlock, textMask], {yPercent: -101}, 0)
+
+            .to(text, {y: 0}, 0)
+
+
+    }
+
+    return tl;
 }
 
 function moveImages(e) {
@@ -86,11 +141,15 @@ function moveImages(e) {
     })
 }
 
+function textHeight(textCopy) {
+    return textCopy.clientHeight
+}
 
 function init() {
 
     initNavigation();
     initHeaderTilt();
+    initHoverRevel();
 
 }
 
